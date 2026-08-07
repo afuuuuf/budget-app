7ca628d8961e8fcd1928887c0971be6fe378dffd
afuuuuf <wnafif02@gmail.com>
2026-08-07 21:22:46 +0800

    feat: CORS

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,14 +1,14 @@
 from pydantic_settings import BaseSettings
-
+from typing import List
 
 class Settings(BaseSettings):
     DATABASE_URL: str
     HOST: str = "127.0.0.1"
     PORT: int = 8000
     RELOAD: bool = True
+    CORS_ORIGINS: List[str] = ["http://localhost:3000"]
 
     class Config:
         env_file = ".env"
-
 
 settings = Settings()
--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
-from .routers import transactions_routers, budget_routers
-from .handler import ExceptionHandler
+from fastapi.middleware.cors import CORSMiddleware
+
 from contextlib import asynccontextmanager
 from alembic.config import Config
 from alembic import command
+
+from .routers import transactions_routers, budget_routers
+from .handler import ExceptionHandler
+from .config import settings
 
 def run_migrations():
     alembic_cfg = Config("alembic.ini")
@@ -14,7 +18,16 @@ async def lifespan(app: FastAPI):
     run_migrations()
     yield
 
-app = FastAPI(title="Personal Budget API")
+app = FastAPI(title="Personal Budget API", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 ExceptionHandler.register_exception_handlers(app)
 
 app.include_router(transactions_routers.router)
