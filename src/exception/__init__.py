5e668a8d146db3bbed6868a95b4a553b263a050d
afuuuuf <wnafif02@gmail.com>
2026-08-11 19:34:03 +0800

    feat: enhance budget creation and updates

--- src/exception/__init__.py
+++ src/exception/__init__.py
@@ -1,5 +1,6 @@
 from .not_found_exception import NotFoundException
+from .unsupported_action_exception import UnsupportedActionException
 
 __all__ = [
-    "NotFoundException"
+    "NotFoundException", "UnsupportedActionException"
 ]
